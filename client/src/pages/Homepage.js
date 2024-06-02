0c4f0579dfdb6846dda35b06478cdece3d65d82c
Jester Galino <hunyo9478@gmail.com>
2024-06-02 19:54:47 +0800

    updated link

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -479,7 +479,8 @@ function DrawerAppBar(props) {
                 >
                   <Image src="../images/new-qr.png" alt="qr-code.jpg" />
                   <Link
-                    to="https://drive.google.com/file/d/1AtP9mvz948k5HXUZWL63BO6P9MoPKDlK/view?usp=sharing"
+                    // to="https://drive.google.com/file/d/1AtP9mvz948k5HXUZWL63BO6P9MoPKDlK/view?usp=sharing"
+                    to="https://drive.usercontent.google.com/download?id=1AtP9mvz948k5HXUZWL63BO6P9MoPKDlK&export=download&authuser=0"
                     underline="always"
                     sx={{ textAlign: "center" }}
                     target="_blank"
