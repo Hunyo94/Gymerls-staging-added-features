20d07a71e1ecfd1c225b0eaffb151972d9e0804d
Jester Galino <hunyo9478@gmail.com>
2024-06-28 08:22:13 +0800

    test

--- api/index.js
+++ api/index.js
@@ -32,7 +32,7 @@ var db = mysql.createPool({
   connectionLimit: 10,
   host: "mysql-176444-0.cloudclusters.net",
   user: "admin",
-  password: "nEEArVSF",
+  password: "nEEArVS",
   database: "gymerls_db",
   port: "15828",
 });
