7d5b5713add496d9215a79ec41d8967153abdb93
Jester Galino <hunyo9478@gmail.com>
2024-07-22 09:32:47 +0800

    updated api

--- api/index.js
+++ api/index.js
@@ -30,11 +30,11 @@ app.use(express.json());
 // LIVE ENV
 var db = mysql.createPool({
   connectionLimit: 10,
-  host: "mysql-178481-0.cloudclusters.net",
+  host: "mysql-179210-0.cloudclusters.net",
   user: "admin",
-  password: "IO0g0peg",
+  password: "17V8zSeG",
   database: "gymerls_db",
-  port: "19924",
+  port: "10026",
 });
 
 app.get("/", function (req, res) {
